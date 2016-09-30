28ba31de34ad9eb2c7581f47ed64de6ed756d748
Denis Korkhov <sqdron.io@gmail.com>
2016-09-30 19:01:04 +0300

    Nats endpoint

--- main.go
+++ main.go
@@ -1,20 +1,18 @@
 package main
 
 import (
-	"log"
 	"net/http"
 	"fmt"
 	"github.com/sqdron/squad-gateway/server"
-	"github.com/nats-io/nats"
+	//"github.com/nats-io/nats"
 	"github.com/sqdron/squad/configurator"
+	"github.com/sqdron/squad/endpoint/nats"
+	"github.com/sqdron/squad/endpoint"
+	"log"
 )
 
 type GateMux struct {
 	Url string
-}
-
-type Message struct {
-	Path string
 }
 
 func (p *GateMux) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -28,19 +26,11 @@ func (p *GateMux) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Access denied !")
 		return
 	}
-
-	log.Println(r.URL.Path + auth)
-	nc, _ := nats.Connect(p.Url)
-	ec, _ := nats.NewEncodedConn(nc, nats.JSON_ENCODER)
-	defer ec.Close()
-
-	m := &Message{Path:r.URL.Path}
-	sendCh := make(chan *Message)
-	ec.BindSendChan("hello", sendCh)
-
-	sendCh <- m
-	fmt.Fprintf(w, "success")
-	nc.Close()
+	log.Println("Url... " + r.URL.Path)
+	nt := nats.NatsEndpoint(p.Url)
+	m := &endpoint.Message{}
+	nt.Publish("hello") <- m
+	nt.Close()
 	return
 }
 
@@ -51,32 +41,33 @@ func main() {
 	cfg := configurator.New()
 	cfg.ReadFlags(opts)
 
-	nc, _ := nats.Connect(opts.Url)
 
-	ec, _ := nats.NewEncodedConn(nc, nats.JSON_ENCODER)
-	defer ec.Close()
-
-	type person struct {
-		Name    string
-		Address string
-		Age     int
-	}
-
-	recvCh := make(chan *person)
-	ec.BindRecvChan("hello", recvCh)
-
-	sendCh := make(chan *person)
-	ec.BindSendChan("hello", sendCh)
-
-	me := &person{Name: "derek", Age: 22, Address: "140 New Montgomery Street"}
-
-	// Send via Go channels
-	sendCh <- me
-
-	// Receive via Go channels
-	who := <-recvCh
-	log.Println(who)
-
+	//nc, _ := nats.Connect(opts.Url)
+	//
+	//ec, _ := nats.NewEncodedConn(nc, nats.JSON_ENCODER)
+	//defer ec.Close()
+	//
+	//type person struct {
+	//	Name    string
+	//	Address string
+	//	Age     int
+	//}
+	//
+	//recvCh := make(chan *person)
+	//ec.BindRecvChan("hello", recvCh)
+	//
+	//sendCh := make(chan *person)
+	//ec.BindSendChan("hello", sendCh)
+	//
+	//me := &person{Name: "derek", Age: 22, Address: "140 New Montgomery Street"}
+	//
+	//// Send via Go channels
+	//sendCh <- me
+	//
+	//// Receive via Go channels
+	//who := <-recvCh
+	//log.Println(who)
+	//
 	log.Println("Listening...")
 	mux := &GateMux{Url:opts.Url}
 	http.ListenAndServe(":3000", mux)
