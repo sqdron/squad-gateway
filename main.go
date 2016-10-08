98600ebaca62666cf8f29751635c09f5d227d726
Denis Korkhov <sqdron.io@gmail.com>
2016-10-08 17:38:15 +0300

    Gateway implementation

--- main.go
+++ main.go
@@ -1,97 +1,46 @@
 package main
 
 import (
+	"github.com/sqdron/squad"
+	"github.com/sqdron/squad/activation"
 	"net/http"
-	"fmt"
-	"github.com/sqdron/squad-gateway/server"
-	"github.com/sqdron/squad/configurator"
 	"log"
-	"github.com/sqdron/squad/endpoint/nats"
+	"fmt"
+	"github.com/nats-io/nats"
+	"encoding/json"
+	"time"
 )
 
-type GateMux struct {
-	Url string
-}
-
-type TokenRequest struct {
-				 //	ClientId     string
-				 //	ClientSecret string   //     `json:"client_secret"`
-	State string
-	Code  string // `json:"code"`
-}
-
-type TokenResponce struct {
-	url string
-}
-
-func (p *GateMux) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	//if (r.Method == "GET"){
-	//	fmt.Fprintf(w, "Access denied for method GET!")
-	//	return
-	//}
-	//
-	//auth:= r.Header.Get("Authorization");
-	//if (auth == "") {
-	//	fmt.Fprintf(w, "Access denied !")
-	//	return
-	//}
-	var path = r.URL.Path[1: len(r.URL.Path)]
-	log.Println("Url... " + path)
-
-	req := &TokenRequest{Code:"", State:""}
-	nt := nats.NatsEndpoint(p.Url)
-	resp := <-nt.Request(path, req)
-	fmt.Println("Got responce")
-	fmt.Println(resp)
-	nt.Close()
-	return
-}
-
 func main() {
+	squad := squad.Client()
+	squad.Activate(func(i activation.ServiceInfo) {
+		connect, _ := nats.Connect(i.Endpoint)
+		http.ListenAndServe(":4000", &SquadMux{connect})
+	})
+}
 
-	opts := &server.Options{}
-	fmt.Println("Read configuration...")
-	cfg := configurator.New()
-	cfg.ReadFlags(opts)
+type SquadMux struct {
+	connect *nats.Conn
+}
 
-	//squad.Client(opts.Hub, opts.ApplicationID).Activate()
+func (s *SquadMux) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	request := map[string]string{}
+	var path = r.URL.Path[1: len(r.URL.Path)]
+	fmt.Println(r)
+	log.Printf("Route to %s \n", path)
 
-	//client.Hub().
-	//
-	//client.Activate()
-	//
-	//nc, _ := nats.Connect(opts.Url)
-	//
-	//ec, _ := nats.NewEncodedConn(nc, nats.JSON_ENCODER)
-	//defer ec.Close()
-	//
-	//type person struct {
-	//	Name    string
-	//	Address string
-	//	Age     int
-	//}
-	//
-	//recvCh := make(chan *person)
-	//ec.BindRecvChan("hello", recvCh)
-	//
-	//sendCh := make(chan *person)
-	//ec.BindSendChan("hello", sendCh)
-	//
-	//me := &person{Name: "derek", Age: 22, Address: "140 New Montgomery Street"}
-	//
-	//// Send via Go channels
-	//sendCh <- me
-	//
-	//// Receive via Go channels
-	//who := <-recvCh
-	//log.Println(who)
+	if (r.Method == http.MethodGet) {
+		for k, v := range r.URL.Query() {
+			request[k] = v[0]
+		}
+	}
+	log.Printf("Requesting %s with params %s\n", path, request)
 
-	log.Println("Listening...")
-	mux := &GateMux{Url:opts.Hub}
-	http.ListenAndServe(":4000", mux)
+	data, encodeError := json.Marshal(request)
+	if (encodeError != nil) {
+		panic(encodeError)
+	}
 
-	//nt := nats.NatsEndpoint(opts.Hub)
-	//m := &endpoint.Message{}
-	//nt.Publish("auth") <- m
-	//nt.Close()
+	res, _ := s.connect.Request("get_auth_url", data, 10 * time.Millisecond)
+	log.Printf("Requestis %s \n", res)
 }
