8a6870f124d609f76c4d5ea9ab92637e0f8a89c8
Denis Korkhov <sqdron.io@gmail.com>
2016-10-09 15:30:04 +0300

    Basic Auth for gateway

--- main.go
+++ main.go
@@ -5,29 +5,47 @@ import (
 	"github.com/sqdron/squad/activation"
 	"net/http"
 	"log"
-	"github.com/nats-io/nats"
 	"github.com/sqdron/squad/connect"
 	"time"
+	"github.com/sqdron/squad/configurator"
+	"strings"
 )
 
+type GatewayOptions struct {
+	AuthToken string `json:"auth_token"`
+}
+
 func main() {
+	op := &GatewayOptions{}
+
+	//TODO: this data should be loaded from hab
+	configurator.New().ReadFromFile("gateway.json", &op)
+
 	squad := squad.Client()
 	squad.Activate(func(i activation.ServiceInfo) {
-		con, e := nats.Connect(i.Endpoint)
-		if (e != nil) {
-			panic(e)
-		}
-		c := connect.NewTransport(i.Endpoint)
-		http.ListenAndServe(":4000", &SquadMux{connect:con, c:c})
+		http.ListenAndServe(":4000", &SquadMux{Options:op, Connect:connect.NewTransport(i.Endpoint)})
 	})
 }
 
 type SquadMux struct {
-	connect *nats.Conn
-	c       connect.ITransport
+	Connect connect.ITransport
+	Options *GatewayOptions
+}
+
+func (s *SquadMux) checkAuth(r *http.Request) bool {
+	auth := strings.SplitN(r.Header.Get("Authorization"), " ", 2)
+	if len(auth) != 2 {
+		return false
+	}
+	return auth[1] == s.Options.AuthToken
 }
 
 func (s *SquadMux) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if (!s.checkAuth(r)) {
+		http.Error(w, "Unauthorized", http.StatusUnauthorized)
+		return
+	}
+
 	request := map[string]string{}
 	var path = r.URL.Path[1: len(r.URL.Path)]
 	if (r.Method == http.MethodGet) {
@@ -36,11 +54,12 @@ func (s *SquadMux) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 	log.Printf("Requesting %s with params %s\n", path, request)
-	res, e := s.c.RequestSync(path, request, 1 * time.Second)
+	res, e := s.Connect.RequestSync(path, request, 1 * time.Second)
 	if (e != nil ) {
 
 		http.Error(w, e.Error(), http.StatusInternalServerError)
 		log.Println(e)
+		return
 	}
 
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
