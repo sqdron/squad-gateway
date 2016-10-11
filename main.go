36a73bc26a7db14815a9f43581c47fb79780e15a
Denis Korkhov <sqdron.io@gmail.com>
2016-10-11 14:46:08 +0300

    auth providerDeployment configuration

--- main.go
+++ main.go
@@ -41,7 +41,19 @@ func (s *SquadMux) checkAuth(r *http.Request) bool {
 	return auth[1] == s.Options.AuthToken
 }
 
+//TODO: Refactor usin middleware approach
 func (s *SquadMux) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if origin := r.Header.Get("Origin"); origin != "" {
+		w.Header().Set("Access-Control-Allow-Origin", origin)
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers",
+			"Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+	}
+	// Stop here if its Preflighted OPTIONS request
+	if r.Method == "OPTIONS" {
+		return
+	}
+
 	if (!s.checkAuth(r)) {
 		http.Error(w, "Unauthorized", http.StatusUnauthorized)
 		return
